4a2a1597fcdb76ebaf0e0bab9b20de40d16a39e4
joaoFerreiragHub <96747056+joaoFerreiragHub@users.noreply.github.com>
2022-10-23 22:26:28 +0100

    updates

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,11 +11,11 @@ const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
-
     let updatedItems;
 
     if (existingCartItem) {
@@ -24,9 +24,8 @@ const cartReducer = (state, action) => {
         amount: existingCartItem.amount + action.item.amount,
       };
       updatedItems = [...state.items];
-      updatedItems(existingCartItemIndex)=updatedItem;
-    }else{
-
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
       updatedItems = state.items.concat(action.item);
     }
 
